83ed1be164303724902cf8abef8321c3ed32287a
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-28 00:14:51 +0630

    Format Code

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Auth/LoginRequestModel.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Auth/LoginRequestModel.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Auth;
 
-namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Auth
+public class LoginRequestModel
 {
-    public class LoginRequestModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
