c6a8b713c9ad33f476fc46733c5bf99831376064
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-28 00:17:23 +0630

    Format Code

--- DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/Entities/TblOrderDetail.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/Entities/TblOrderDetail.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.Entities;
+﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.Entities;
 
 public partial class TblOrderDetail
 {
