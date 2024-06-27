134fa38a0c6e4cf3e354fc9db8a0caa45522b39a
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-28 00:12:49 +0630

    Format Code

--- DotNet8.CleanArchitectureWithUnitOfWork.Application/Services/DapperService.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Application/Services/DapperService.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Application.Services;
 
-namespace DotNet8.CleanArchitectureWithUnitOfWork.Application.Services
+public class DapperService
 {
-    public class DapperService
-    {
-    }
 }
