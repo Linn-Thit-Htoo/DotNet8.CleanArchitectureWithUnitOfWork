58c05da4437466bd41ace9539514a07d14a8091e
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-28 00:14:43 +0630

    Format Code

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Auth/AuthResponseModel.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Auth/AuthResponseModel.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Auth;
 
-namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Auth
+public class AuthResponseModel
 {
-    public class AuthResponseModel
-    {
-    }
 }
