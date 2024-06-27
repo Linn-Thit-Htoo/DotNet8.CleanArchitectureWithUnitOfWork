0786eae401233840518c9fe2f6e3ee981901b4f0
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-28 00:16:08 +0630

    Format Code

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/GlobalUsings.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/GlobalUsings.cs
@@ -1,5 +1,6 @@
 // Global using directives
 
+global using DotNet8.CleanArchitectureWithUnitOfWork.Domain.Enums;
 global using DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models;
 global using DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Auth;
 global using DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Blog;
