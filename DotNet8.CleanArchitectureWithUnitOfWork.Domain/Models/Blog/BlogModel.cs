bb95e5bae27e6a2e3a172504eaa87618992b8ab3
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-28 00:15:43 +0630

    Format Code

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Blog/BlogModel.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Blog/BlogModel.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Blog;
 
-namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Blog
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public int BlogId { get; set; }
+    public int BlogId { get; set; }
 
-        public string? BlogTitle { get; set; }
+    public string? BlogTitle { get; set; }
 
-        public string? BlogAuthor { get; set; }
+    public string? BlogAuthor { get; set; }
 
-        public string? BlogContent { get; set; }
-    }
+    public string? BlogContent { get; set; }
 }
