cc81c20891bd3f11c3e2ac26fe1d0bce27a17c0d
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-28 00:17:42 +0630

    Format Code

--- DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/Repositories/BlogRepository.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure/Repositories/BlogRepository.cs
@@ -3,47 +3,41 @@ using DotNet8.CleanArchitectureWithUnitOfWork.Domain.Interfaces;
 using DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models;
 using DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Blog;
 using DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.Repositories
+namespace DotNet8.CleanArchitectureWithUnitOfWork.Infrastructure.Repositories;
+
+public class BlogRepository : IBlogRepository
 {
-    public class BlogRepository : IBlogRepository
+    private readonly AppDbContext _context;
+
+    public BlogRepository(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public BlogRepository(AppDbContext context)
+    public Task<Result<BlogResponseModel>> GetBlog(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Result<BlogListResponseModel>> GetBlogs()
+    {
+        Result<BlogListResponseModel> responseModel;
+        try
         {
-            _context = context;
+            var lst = await _context.TblBlogs
+                .OrderByDescending(x => x.BlogId)
+                .ToListAsync();
+
+            var model = new BlogListResponseModel { Blogs = lst.Select(x => x.Change()).ToList() };
+
+            responseModel = Result<BlogListResponseModel>.SuccessResult(model);
+        }
+        catch (Exception ex)
+        {
+            responseModel = Result<BlogListResponseModel>.FailureResult(ex);
         }
 
-        public Task<Result<BlogResponseModel>> GetBlog(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<Result<BlogListResponseModel>> GetBlogs()
-        {
-            Result<BlogListResponseModel> responseModel;
-            try
-            {
-                var lst = await _context.TblBlogs
-                    .OrderByDescending(x => x.BlogId)
-                    .ToListAsync();
-
-                var model = new BlogListResponseModel { Blogs = lst.Select(x => x.Change()).ToList() };
-
-                responseModel = Result<BlogListResponseModel>.SuccessResult(model);
-            }
-            catch (Exception ex)
-            {
-                responseModel = Result<BlogListResponseModel>.FailureResult(ex);
-            }
-
-            return responseModel;
-        }
+        return responseModel;
     }
 }
