5a570b22b3fd6052cf162b133f3899f9b84d19f9
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-28 00:15:24 +0630

    Format Code

--- DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Auth/UserModel.cs
+++ DotNet8.CleanArchitectureWithUnitOfWork.Domain/Models/Auth/UserModel.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Auth;
 
-namespace DotNet8.CleanArchitectureWithUnitOfWork.Domain.Models.Auth
+public class UserModel
 {
-    public class UserModel
-    {
-        public string UesrId { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string UserRole { get;set; }
-        public bool IsActive { get; set; }
-        public string CreatedDate { get; set; }
-    }
+    public string UesrId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string UserRole { get;set; }
+    public bool IsActive { get; set; }
+    public string CreatedDate { get; set; }
 }
